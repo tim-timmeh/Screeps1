d66f6f5672ef757531497c11714eb8c8ceb56816
Coffey <tim.coffey@woodside.com.au>
2020-01-09 16:40:53 +0800

    end of day

--- prototypes.js
+++ prototypes.js
@@ -1,3 +1,5 @@
-StructureSpawn.prototype.spawnTest = function() {
+StructureSpawn.prototype.spawnTest = function() { // Test function
   console.log(this.name + ' spawnTest');
 };
+
+Source.prototype.memory = undefined;
